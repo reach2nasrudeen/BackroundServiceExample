939e9232a67f09ca1e33b835efbdb937eab6aec4
Nasrudeen <nasrudeen@madebyfire.com>
2016-10-06 11:07:48 +0530

    Service Startup on Boot Added

--- app/src/main/java/com/nasrudeen/serviceexample/MyReceiver.java
+++ app/src/main/java/com/nasrudeen/serviceexample/MyReceiver.java
@@ -3,6 +3,7 @@ package com.nasrudeen.serviceexample;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
     public MyReceiver() {
@@ -14,8 +15,11 @@ public class MyReceiver extends BroadcastReceiver {
         // an Intent broadcast.
         if ((intent.getAction().equals(Intent.ACTION_USER_PRESENT))
                 || (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED))) {
-            startService(new Intent(getBaseContext(), MyService.class));
+//            Intent i = new Intent(context, MyService.class);
+//            context.startService(i);
+            Toast.makeText(context,"Test service",Toast.LENGTH_SHORT).show();
+            context.startService(new Intent(context, MyService.class));
         }
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
     }
 }
--- app/src/main/java/com/nasrudeen/serviceexample/SampleReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.nasrudeen.serviceexample;
-
-/**
- * Created by Nasrudeen on 9/17/2016.
- */
-public class SampleReceiver extends BroadcastReceiver {
-}
